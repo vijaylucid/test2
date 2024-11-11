ffac45d0c4be120d8b5614e41f564e6feeba93d7
vijaylucid <vijay@lucidits.com>
2024-11-11 17:35:26 +0530

    new js file has been created

---
+++ test.js
@@ -0,0 +1,15 @@
+// Import necessary dependencies
+import React from 'react';
+import { withRouter } from 'react-router-dom';
+
+// Define the functional component
+function Test() {
+    return (
+        <div>
+            <h1>Hello from the Test Component</h1>
+        </div>
+    );
+}
+
+// Export the component wrapped with withRouter for route access
+export default withRouter(Test);
